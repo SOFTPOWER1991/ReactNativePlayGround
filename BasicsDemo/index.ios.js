5f7c4211fa8aa5ae786518c52da8ed040a24521a
zhanggeng <zhanggengdyx@163.com>
2016-08-31 22:52:58 +0800

    add a new post about : flex , width , height

---
+++ BasicsDemo/index.ios.js
@@ -0,0 +1,40 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+class BasicsDemo extends Component {
+  render() {
+    return (
+      // <View style={styles.container}>
+      //   <View style={{width:50, height:50,backgroundColor:'red'}}/>
+      //   <View style={{width:100, height:100,backgroundColor:'green'}}/>
+      //   <View style={{width:150, height:150,backgroundColor:'skyblue'}}/>
+      // </View>
+
+        <View style={{flex:1}}>
+          <View style={{flex:1,backgroundColor:'red'}}/>
+          <View style={{flex: 2,backgroundColor:'green'}}/>
+          <View style={{flex:2,backgroundColor:'skyblue'}}/>
+        </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f4f4f4'
+  }
+});
+
+AppRegistry.registerComponent('BasicsDemo', () => BasicsDemo);
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # ReactNativePlayGround
-React Native Play Ground
+React Native Play Ground for practice。
+
+## 高和宽
+
+* 确定大小 
+
+	给每一个view指定一个固定的width,height：他们没有单位，代表密度无关的像素；
+	
+	```
+	<View>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}} />
+        <View style={{width: 100, height: 100, backgroundColor: 'skyblue'}} />
+        <View style={{width: 150, height: 150, backgroundColor: 'steelblue'}} />
+      </View>
+	```
+* 弹性大小
+
+	使用flex，跟android中的weight相似；
+	
+	```
+	<View style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}} />
+        <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+        <View style={{flex: 3, backgroundColor: 'steelblue'}} />
+      </View>
+	```
+
+
